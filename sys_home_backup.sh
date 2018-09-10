91c585011b8a02f16d6d15d80f587f9e865b4882
Adam Deacon <adeacon@findmypast.com>
2018-09-10 15:31:09 +0100

    Parameterised sync script

--- sys_home_backup.sh
+++ sys_home_backup.sh
@@ -4,14 +4,14 @@ REMOTE_STORAGE=/drives/E/BLAH/
 REMOTE_STORAGE2=/drives/E/BLAH/
 declare -a FOLDER_LIST=("BLAH" "BLAH" "BLAH" "BLAH")
 
-# #Just in case - backup E:
-# echo rsync -avh --progress ${REMOTE_STORAGE} ${REMOTE_STORAGE2}
+##Just in case - backup E:
+# rsync -avh --progress ${REMOTE_STORAGE} ${REMOTE_STORAGE2}
 
 for i in "${FOLDER_LIST[@]}"; do
 	echo "$i"
-	# #Best of both worlds - fill gaps on C: and E:
-	# echo rsync -avhu --progress ${LOCAL_STORAGE}${i}/ ${REMOTE_STORAGE}${i}/
-	# echo rsync -avhu --progress ${REMOTE_STORAGE}${i}/ ${LOCAL_STORAGE}${i}/
-	#Perfect harmony - replicate C: on E:
-	echo rsync -avh --delete --progress ${LOCAL_STORAGE}${i}/ ${REMOTE_STORAGE}${i}/
+	##Best of both worlds - fill gaps on C: and E:
+	# rsync -avhu --progress ${LOCAL_STORAGE}${i}/ ${REMOTE_STORAGE}${i}/
+	# rsync -avhu --progress ${REMOTE_STORAGE}${i}/ ${LOCAL_STORAGE}${i}/
+	##Perfect harmony - replicate C: on E:
+	rsync -avh --delete --progress ${LOCAL_STORAGE}${i}/ ${REMOTE_STORAGE}${i}/
 done
